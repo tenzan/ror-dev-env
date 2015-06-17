db7f1b1db3113dd330f0970700629dc9c53df7b9
Askarbek Karasaev <askar.karasaev@gmail.com>
2015-06-17 15:33:09 +0900

    permission fix for work directory

--- provision.sh
+++ provision.sh
@@ -9,6 +9,8 @@
 # Exit on error
 set -e
 
+sudo chown vagrant:vagrant ~/work
+
 sudo apt-get update
 sudo apt-get install -y git-core curl zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev python-software-properties libffi-dev
 sudo apt-get install -y libgdbm-dev libncurses5-dev automake libtool bison libffi-dev
